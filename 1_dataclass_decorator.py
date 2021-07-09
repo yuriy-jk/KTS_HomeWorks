8bf892ffcf05c29668246fe9f8149771d2020e82
yuriy-jk <complete@bk.ru>
2021-07-09 22:37:03 +0300

    first

---
+++ 1_dataclass_decorator.py
@@ -0,0 +1,33 @@
+from marshmallow import Schema, fields
+from marshmallow.schema import SchemaMeta
+
+
+class MyMetaClass(SchemaMeta):
+
+    def __new__(mcs, name, bases, classdict):
+        for k, v in classdict.items():
+            classdict[k] = Schema.TYPE_MAPPING[classdict[k]]()
+        cls = super().__new__(mcs, name, bases, classdict)
+        return cls
+
+
+def my_dataclass(cls):
+    types = cls.__annotations__
+
+    def schema():
+        cls.Schema = MyMetaClass('User', (Schema,), types)
+        return cls
+
+    return schema()
+
+
+@my_dataclass
+class User:
+    username: str
+    age: int
+    superuser: bool
+
+
+print(User.Schema)
+print(User.Schema().load({'username': 'tod', 'age': 18}))
+print(User.Schema().load({'username': 'tod', 'age': 18, 'superuser': False}))
