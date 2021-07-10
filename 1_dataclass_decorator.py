96f7face4c2e4d09d1466d6aa1e2e68cad22ab1e
yuriy-jk <complete@bk.ru>
2021-07-10 20:13:41 +0300

    typings_and_chekckings

--- 1_dataclass_decorator.py
+++ 1_dataclass_decorator.py
@@ -1,20 +1,29 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema
 from marshmallow.schema import SchemaMeta
+from typing import Any, Callable, TypeVar, Dict
+
+ClassUser = TypeVar('ClassUser')
 
 
 class MyMetaClass(SchemaMeta):
 
-    def __new__(mcs, name, bases, classdict):
+    def __new__(mcs, name: str, bases: tuple, classdict: Dict):
         for key in classdict:
             classdict[key] = Schema.TYPE_MAPPING[classdict[key]]()
         cls = super().__new__(mcs, name, bases, classdict)
         return cls
 
 
-def my_dataclass(cls):
+def my_dataclass(cls: Any) -> Callable:
+    """
+    decorator for User
+    build User.Scheme class
+    validate User's annotaions fields with marsmallow.schema fields
+    """
+
     types = cls.__annotations__
 
-    def schema():
+    def schema() -> ClassUser:
         cls.Schema = MyMetaClass('User', (Schema,), types)
         return cls
 
